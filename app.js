dbde0e7835e2574cf37568b96d1808876f2d7566
coolroy69 <112404216+coolroy69@users.noreply.github.com>
2022-09-18 20:53:32 +0530

    hvb
    vv

---
+++ app.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector("burger")
+const navLinks = document.querySelector("nav-links")
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+
+})
