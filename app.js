893fa9f008300651e5c09c524558528647b9b1f3
coolroy69 <112404216+coolroy69@users.noreply.github.com>
2022-09-19 21:59:37 +0530

    final
    only little java remaining

--- app.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector("burger")
-const navLinks = document.querySelector("nav-links")
-
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    nav.classList.toggle("active");
-
-})
